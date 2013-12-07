2eeef1edfcedee561120ca95eff98e8364992948
kushjain@thoughtworks.com <Mr.KushJain@gmail.com>
2013-12-07 16:29:12 +0530

    #6|AA KJ|Added migration for creating incidents table

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20131207103608) do
+
+  create_table "incidents", :force => true do |t|
+    t.string   "location",           :null => false
+    t.datetime "date_of_incident",   :null => false
+    t.string   "type_of_harassment", :null => false
+    t.string   "description"
+    t.integer  "age",                :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
 
 end
