e35c92606af9ee5c4d1fabc34cb191194b8a9479
kushjain@thoughtworks.com <Mr.KushJain@gmail.com>
2013-12-07 13:54:13 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shealert::Application.config.secret_token = '12989aa5e27184b9810f367b8777ce646500f8e2adf1a684a5ab524a9ed6993ecbb3e6d53549dd64e342514bea083a16310c949424fcd21560d5969b622dd95c'
---
+++ db/db.sql
@@ -0,0 +1,5 @@
+create role shealert_development with createdb login password 'password';
+create role shealert_test with createdb login password 'password';
+create role shealert_production with createdb login password 'password';
+create role shealert with createdb login password 'password';
+
